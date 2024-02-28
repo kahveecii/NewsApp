483b568d244a894a8abe857c15bb9bd406c09414
Ahmed Deehan <deehankahveci13@gmail.com>
2024-02-28 15:03:29 +0700

    new

---
+++ database/migrations/2024_02_28_065917_create_news_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            //relationship with category
+            $table->foreignId('category_id');
+            $table->string('');
+            $table->string('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+};
